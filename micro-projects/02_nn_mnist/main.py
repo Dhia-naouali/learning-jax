9f8a7878a35b8fc2d0effb14dd481c98bbfdfd2f
Dhia Naouali <dhia12naouali@gmail.com>
2025-08-26 00:13:58 +0100

    typos

--- micro-projects/02_nn_mnist/main.py
+++ micro-projects/02_nn_mnist/main.py
@@ -1,36 +1,39 @@
 import os
+import wandb
+import numpy as np
+import collections
+from tqdm import tqdm
+
 import jax
 import optax
 import jaxopt
-import collections
 import jax.numpy as jnp
-from torch.utils.data import DataLoader
-from torchvision import datasets
 
-import wandb
-from tqdm import tqdm
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
 
 from model import NN
 
 def setup_data():
-    train_dataset = datasets.MNIST("data", download=True, train=True)
-    val_dataset = datasets.MNIST("data", download=True, train=False)
+    train_dataset = datasets.MNIST("data", download=True, train=True, transform=transforms.ToTensor())
+    val_dataset = datasets.MNIST("data", download=True, train=False, transform=transforms.ToTensor())
     
     train_loader = DataLoader(
         train_dataset,
         batch_size=32,
         shuffle=True,
-        num_workers=os.cpu_counts()
+        num_workers=os.cpu_count()
     )
     
     val_loader = DataLoader(
         val_dataset,
         batch_size=128,
         shuffle=False,
-        num_workers=os.cpu_counts()
+        num_workers=os.cpu_count()
     )
     
-    return train_loader, val_loader, (28, 28, 3), 10
+    return train_loader, val_loader, (784,), 10
 
 
 def main():
@@ -38,8 +41,9 @@ def main():
     train_loader, val_loader, input_shape, num_classes = setup_data()
     model = NN(hidden_dim=64, num_classes=num_classes)
     rng_init, _ = jax.random.split(jax.random.PRNGKey(0))
-    init_vars = model.init(rng_init, jnp.zeros((1,) + input_shape))
-    optimizer = optax.sgd()
+    params = model.init(rng_init, jnp.zeros((1,) + input_shape))
+    optimizer = optax.sgd(1e-3)
+    optimizer_state = optimizer.init(params)
     log_loss = jax.vmap(jaxopt.loss.multiclass_logistic_loss)
     
     def loss_fn(params, images, labels):
@@ -49,34 +53,34 @@ def main():
     
     @jax.jit
     def train_step(params, optimizer_state, images, labels):
-        loss_grad = jax.grad(loss_fn, hax_aux=True)
-        logits, grads = loss_grad(params, images, labels)
+        grads, logits = jax.grad(loss_fn, has_aux=True)(params, images, labels)
         updates, optimizer_state = optimizer.update(grads, optimizer_state)
         params = optax.apply_updates(params, updates)
         return params, optimizer_state, logits
     
     for epoch in range(1, 13):
-        metrics = collection.defaultdict(list)
+        metrics = collections.defaultdict(list)
         pb = tqdm(train_loader, f"epoch {epoch}/{12}")
-        images, labels = jnp.asarray(images.numpy()), jnp.asarray(labels.numpy())
-        images = jnp.moveaxis(images, -3, -1)
-        params, optimizer_state, logits = train_step(params, optimizer_state, images, labels)
-        loss = log_loss(labels, logits)
-        preds = jnp.argmax(logits, axis=-1)
-        acc = (preds == labels)
-        metrics["train_acc"].append(acc)
-        metrics["train_loss"].append(loss)
-        
+        for images, labels in pb:
+            images, labels = jnp.asarray(images.numpy()), jnp.asarray(labels.numpy())
+            images = jnp.reshape(images, (-1, 784))
+            params, optimizer_state, logits = train_step(params, optimizer_state, images, labels)
+            loss = log_loss(labels, logits)
+            preds = jnp.argmax(logits, axis=-1)
+            acc = (preds == labels)
+            metrics["train_acc"].append(acc)
+            metrics["train_loss"].append(loss)
+            
         metrics.update({
             k: np.concatenate(v).mean()
             for k, v in metrics.items()
         })
-        
+            
         pb = tqdm(val_loader, "val")
-        for image, labels in pb:
+        for images, labels in pb:
             images, labels = jnp.asarray(images.numpy()), jnp.asarray(labels.numpy())
-            images = jnp.moveaxis(images, -3, -1)
-            logits = model(params, images)
+            images = jnp.reshape(images, (-1, 784))
+            logits = model.apply(params, images)
             loss = log_loss(labels, logits)
             
             preds = jnp.argmax(logits, axis=-1)
@@ -84,10 +88,11 @@ def main():
             metrics["val_acc"].append(acc)
             metrics["val_loss"].append(loss)
             
-        metrics.update([
+        metrics.update({
+            
             k: np.concatenate(v).mean()
             for k, v in metrics.items() if k.startswith("val")   
-        ])
+        })
         
         wandb.log(metrics)
         
