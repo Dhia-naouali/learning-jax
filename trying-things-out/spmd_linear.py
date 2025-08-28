ca847d91fd3ccee1252d0ea893c565f57a1c959f
Dhia Naouali <dhia12naouali@gmail.com>
2025-08-28 04:21:36 +0100

    parallel linear, sharded x, replicate params

--- trying-things-out/spmd_linear.py
+++ trying-things-out/spmd_linear.py
@@ -1,5 +1,6 @@
 import jax
 from jax.sharding import PartitionSpec as P
+import numpy as np
 
 jax.config.update("jax_num_cpu_devices", 4)
 key = jax.random.key(12)
@@ -7,12 +8,17 @@ key, x_key, w_key, b_key = jax.random.split(key, 4)
 
 @jax.jit
 def linear(x, w, b):
-    print(f"{jax.typeof(x) = } {jax.typeof(w) = } {jax.typeof(b) = }")
+    print(f"{jax.typeof(x) = }")
+    print(f"{jax.typeof(w) = }")
+    print(f"{jax.typeof(b) = }")
     return x @ w + b
 
 x = jax.random.uniform(x_key, (256, 32))
 w = jax.random.uniform(w_key, (32, 16))
 b = jax.random.uniform(b_key, (16,))
+x_np = np.array(x)
+w_np = np.array(w)
+b_np = np.array(b)
 
 mesh = jax.make_mesh((4,), ("x",), axis_types=(jax.sharding.AxisType.Explicit,))
 sharding = jax.NamedSharding(mesh, P("x"))
@@ -28,4 +34,5 @@ with jax.set_mesh(mesh):
     y = linear(sharded_x, w, sharded_b)
     
     
-print(y.shape)
+print(f"{y.shape = }")
+print(f"equal to single device results: {(np.array(y) == (x_np @ w_np + b)).all()}")
