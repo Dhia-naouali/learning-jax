8a7ee959baa52c1e25d27e4a40accd6445f3b545
Dhia Naouali <dhia12naouali@gmail.com>
2025-09-09 06:10:11 +0100

    fixes

--- micro-projects/05_distributed_training/train.py
+++ micro-projects/05_distributed_training/train.py
@@ -1,3 +1,4 @@
+import os
 import time
 from functools import partial
 from tqdm import tqdm
@@ -8,9 +9,9 @@ from flax.training import train_state, checkpoints
 import optax
 import flax
 
-from .data_loader import make_loader, transform_batch
-from .model import CNN
-from .utils import shard_batch, unreplicate_state
+from data_loader import make_loader, transform_batch
+from model import CNN
+from utils import shard_batch, unreplicate_state
 
 class TrainState(train_state.TrainState):
     pass
@@ -27,7 +28,7 @@ def create_train_state(rng, model, learning_rate):
 @partial(jax.pmap, axis_name='batch')
 def train_step(state, images, labels):
     def loss_fn(params):
-        logits = state.apply_fn({"params": params}, images, train=True)
+        logits = state.apply_fn({"params": params}, images)
         loss = optax.softmax_cross_entropy(
             logits, 
             jax.nn.one_hot(labels, num_classes=10)
@@ -51,9 +52,10 @@ def main(
     device_batch=256, # TPU cores are chunky
     epochs=3,
     learning_rate=1e-3,
-    ckpt_dir="checkpoints",
+    ckpt_dir="./checkpoints",
     num_workers=None,
 ):
+    ckpt_dir = os.path.abspath(ckpt_dir)
     world_size = jax.local_device_count()
     global_batch = device_batch * world_size
 
@@ -82,9 +84,9 @@ def main(
                 pb.set_postfix(loss=f"{logs['loss']:.4f}", acc=f"{logs['accuracy']:.4f}")
             step += 1
 
-        state_to_save = unreplicate_state(state)
-        checkpoints.save_checkpoint(ckpt_dir, state_to_save, step=step, overwrite=True)
-        print(f"{time.time() - start:.1f}s per epoch")
+    state_to_save = unreplicate_state(state)
+    checkpoints.save_checkpoint(ckpt_dir, state_to_save, step=step, overwrite=True)
+    print(f"{((time.time() - start) / epochs):.1f}s per epoch")
 
 
 if __name__ == "__main__":
